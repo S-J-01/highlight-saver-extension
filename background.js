adaf1251493a072621f4a0c6758c40ed1800f45e
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-07-14 23:40:31 +0530

    fetching highlights in background.js

--- background.js
+++ background.js
@@ -10,9 +10,24 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
       console.log("API key found, would summarise highlight id:", msg.id);
 
-      sendResponse({ ok: true });
-    });
+      chrome.storage.local.get(["highlights"], (highlightsRes) => {
+        const highlights = highlightsRes.highlights || [];
+        const highlight = highlights.find((h) => h.id === msg.id);
 
-    return true;
+        if (!highlight) {
+          sendResponse({ error: "HIGHLIGHT_NOT_FOUND" });
+          return;
+        }
+
+        console.log(
+          "Found highlight:",
+          highlight.text.substring(0, 50) + "..."
+        );
+
+        sendResponse({ ok: true });
+      });
+
+      return true;
+    });
   }
 });
